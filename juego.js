834f18f839e2a940efeeb2adf0b958aff860cb07
MariC564 <marianacardenaa@gmail.com>
2024-03-22 08:54:52 -0500

    temporizador

--- juego.js
+++ juego.js
@@ -58,7 +58,27 @@ function seleccionarTarjeta(i) {
         selecciones = []
     }
 }
+function iniciarTemporizador(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+window.onload = function () {
+    var tiempoRestante = 300; 
+    var display = document.querySelector('#temporizador');
+    iniciarTemporizador(tiempoRestante, display);
+}
 function deseleccionar(selecciones) {
     setTimeout(() => {
         let trasera1 = document.getElementById("trasera" + selecciones[0])
@@ -77,6 +97,5 @@ function deseleccionar(selecciones) {
             trasera1.style.background = "pull"
             trasera2.style.background = "pull"
         }
-    }, 1000);
-    
-}
+    }, 1000);  
+  }
