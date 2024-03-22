c04cc368d764ff84ebe7eca523f17b049191e9f3
perez-jpg <perezestevan2005@gmail.com>
2024-03-22 11:03:41 -0500

    actualizacion

--- juego.js
+++ juego.js
@@ -58,27 +58,6 @@ function seleccionarTarjeta(i) {
         selecciones = []
     }
 }
-function iniciarTemporizador(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-window.onload = function () {
-    var tiempoRestante = 300; 
-    var display = document.querySelector('#temporizador');
-    iniciarTemporizador(tiempoRestante, display);
-}
 function deseleccionar(selecciones) {
     setTimeout(() => {
         let trasera1 = document.getElementById("trasera" + selecciones[0])
